c1ede54ecf198cf0ca3222858ebf5d8b45ed178e
Kaloyan Dimov <kaloyan@grind.bg>
2023-11-12 17:57:49 +0300

    boilerplate and project structure

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    date_default_timezone_set('Europe/Sofia');
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    header('Content-Type: text/html');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/main.css">
+    <title>To Do List</title>
+</head>
+<body>
+    
+
+    <script defer src="./src/main.js" type="module"></script>
+</body>
+</html>
