f1b09ac9bc5d68c2e5258f1bbd10cef7d35ba00e
Kaloyan Dimov <kaloyan@grind.bg>
2023-11-13 01:37:54 +0300

    listing tasks in database

--- src/js/list_tasks.js
+++ src/js/list_tasks.js
@@ -3,7 +3,13 @@ export default (tasks) => {
 
     tasks.forEach(task => {
         list_taks.innerHTML +=
-        ``
+        `<div class="task" id=${task.id}>
+            <h2 class="title">${task.title}</h2>
+            <h3 class="description">${task.description}</h3>
+            <h4 class="timestamp">Created: ${task.created_at}</h4>
+            <h4 class="timestamp">Last Updated: ${task.updated_at}</h4>
+        </div>
+        `
         console.log(task)
 
     })
